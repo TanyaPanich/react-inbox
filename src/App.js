37281a39abe109a98bc895e77a98cb1a82ee7315
Tanya <mellene@gmail.com>
2018-05-12 15:08:19 -0600

    all toolbar buttons work

--- src/App.js
+++ src/App.js
@@ -12,19 +12,101 @@ class App extends Component {
     }
   }
 
-  toggleClass = (msg, nameOfClass) => {
-    const index = this.state.messages.indexOf(msg)
-    let newMessages = this.state.messages
-    newMessages[index][nameOfClass] = !newMessages[index][nameOfClass]
-    this.setState({ messages: newMessages })
+  toggleClass = (message, nameOfClass) => {
+    const index = this.state.messages.indexOf(message)
+    // eslint-disable-next-line
+    this.state.messages[index][nameOfClass] = !this.state.messages[index][nameOfClass]
+    this.setState({ messages: this.state.messages })
   }
+
+  countMsgProps = (property) => {
+    // eslint-disable-next-line
+    let count = 0
+    // eslint-disable-next-line
+    this.state.messages.map(message => {
+      if (message[property])
+        count++
+    })
+    console.log('count of', property, 'is', count)
+    return count
+  }
+
+  bulkSelectUnselect = () => {
+    // eslint-disable-next-line
+      if (this.countMsgProps('selected') < this.state.messages.length) {
+        // eslint-disable-next-line
+        this.state.messages.map(message => {
+          message.selected = true
+        })
+        console.log('Select ALL')
+        // eslint-disable-next-line
+        this.setState({ messages: this.state.messages })
+      } else {
+        // eslint-disable-next-line
+        this.state.messages.map(message => {
+          message.selected = false
+        })
+        console.log('Unselect ALL')
+        // eslint-disable-next-line
+        this.setState({ messages:this.state.messages })
+      }
+    }
+
+    markReadUnread = (booleanValue) => {
+      // eslint-disable-next-line
+      this.state.messages.map(message => {
+        if (message.selected) {
+          message.read = booleanValue
+          console.log('is read? ', booleanValue)
+        }
+      })
+      // eslint-disable-next-line
+      this.setState({ messages:this.state.messages })
+    }
+
+    updateMsgLabels = (label, booleanValue) => {
+      // eslint-disable-next-line
+      this.state.messages.map(message => {
+        if (message.selected) {
+          if (booleanValue && !message.labels.includes(label) && label !== "false") {
+            message.labels.push(label)
+            console.log('--Label--', label, 'is added')
+          }
+          else if (!booleanValue && message.labels.includes(label)) {
+            console.log('--Label--', label, 'is removed')
+            message.labels.splice(message.labels.indexOf(label), 1)
+          }
+        }
+      })
+      this.setState({ messages:this.state.messages })
+    }
+
+    deleteMsg = () => {
+      // eslint-disable-next-line
+      this.state.messages.map(message => {
+        if (message.selected) {
+          this.state.messages.splice(this.state.messages.indexOf(message), 1)
+          console.log('After deletetion we have', this.state.messages.length, 'messages')
+        }
+      })
+      this.setState({ messages:this.state.messages })
+    }
+
 
   render() {
     return (
        <div className='container'>
         <h1>React Inbox</h1>
-        <Toolbar messages={ this.state.messages } />
-        <Messages messages={ this.state.messages } toggleClass={this.toggleClass} />
+        <Toolbar
+          messages={ this.state.messages }
+          countMsgProps={ this.countMsgProps }
+          bulkSelectUnselect={ this.bulkSelectUnselect }
+          markReadUnread={ this.markReadUnread }
+          updateMsgLabels={ this.updateMsgLabels }
+          deleteMsg={ this.deleteMsg } />
+        <Messages
+          messages={ this.state.messages }
+          toggleClass={ this.toggleClass } />
       </div>
     )
   }
--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -3,10 +3,26 @@ import React from 'react'
 class Toolbar extends React.Component {
   constructor(props) {
     super(props)
+    console.log('TOOLBAR props:', this.props)
     this.state = {
-      selectedAll: props.selectedAll
+      messages: this.props.messages,
+      countMsgProps: this.props.countMsgProps,
+      bulkSelectUnselect: this.props.bulkSelectUnselect,
+      markReadUnread: this.props.markReadUnread,
+      updateMsgLabels: this.props.updateMsgLabels,
+      deleteMsg: this.props.deleteMsg
     }
-    console.log('TOOLBAR state--->', this.state)
+  }
+
+
+  checkSelected = (messages) => {
+    if(this.state.countMsgProps('selected') === 0) {
+      return 'fa fa-square-o'
+    } else if (this.state.countMsgProps('selected') < messages.length) {
+      return 'fa fa-minus-square-o'
+    } else {
+      return 'fa fa-check-square-o'
+    }
   }
 
   render() {
@@ -14,37 +30,54 @@ class Toolbar extends React.Component {
       <div className="row toolbar">
         <div className="col-md-12">
           <p className="pull-right">
-            <span className="badge badge">2</span>
+            <span className="badge badge">
+              {this.state.messages.length - this.state.countMsgProps("read")}
+            </span>
             unread messages
           </p>
 
-          <button className="btn btn-default">
-            <i className="fa fa-square-o"></i>
+          <button className="btn btn-default"
+                  onClick={() => {
+                    this.state.bulkSelectUnselect()}}>
+            <i className={this.checkSelected(this.state.messages)}></i>
           </button>
 
-          <button className="btn btn-default">
+          <button className="btn btn-default"
+                  onClick={() => {
+                    this.state.markReadUnread(true)}}>
             Mark As Read
           </button>
 
-          <button className="btn btn-default">
+          <button className="btn btn-default"
+                  onClick={() => {
+                    this.state.markReadUnread(false)}}>
             Mark As Unread
           </button>
 
-          <select className="form-control label-select">
-            <option>Apply label</option>
+          <select className="form-control label-select"
+                  onChange={(e) => {
+                    this.state.updateMsgLabels(e.target.value, true)
+                  }}>
+            <option value={false}>Apply label</option>
             <option value="dev">dev</option>
             <option value="personal">personal</option>
             <option value="gschool">gschool</option>
           </select>
 
-          <select className="form-control label-select">
+          <select className="form-control label-select"
+                  onChange={(e) => {
+                    this.state.updateMsgLabels(e.target.value, false)
+                  }}>
             <option>Remove label</option>
             <option value="dev">dev</option>
             <option value="personal">personal</option>
             <option value="gschool">gschool</option>
           </select>
 
-          <button className="btn btn-default">
+          <button className="btn btn-default"
+                  onClick={() => {
+                    this.state.deleteMsg()
+                  }}>
             <i className="fa fa-trash-o"></i>
           </button>
         </div>
